70191f0828b990c33b0a4e5700ac3a3a3d169a80
kirk86 <kirk.86@walla.com>
2017-04-29 22:57:30 +0100

    init commit :zap:

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ def rank_classifiers(models, X, Y, nb_epochs=2, batch_size=128):
         names.append(name)
         print("\nmodel = {}, mean = {}, std = {}".format(name,
                                                        np.mean(results),
-                                                       np.std(results))
+                                                       np.std(results)))
     # boxplot algorithm comparison
     fig = plt.figure()
     fig.suptitle('Algorithm Comparison')
