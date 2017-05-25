9bfe9d696209f3bedb09ac56bf6183ae11ee701e
kirk86 <kirk86@walla.com>
2017-05-25 20:44:28 +0100

    checkpoint function for deap :zap:

--- utils.py
+++ utils.py
@@ -386,9 +386,21 @@ def eaSimpleModified(population, toolbox, cxpb, mutpb, ngen, stats=None,
     best.append(best_ind)
 
     # Begin the generational process
-    for gen in range(1, ngen + 1):
+    for gen in xrange(1, ngen + 1):
         # Select the next generation individuals
         offspring = toolbox.select(population, len(population))
+
+    #     # Apply crossover and mutation on the offspring
+    #     for child1, child2 in zip(offspring[::2], offspring[1::2]):
+    #         if np.random.random() < cxpb:
+    #             toolbox.mate(child1, child2)
+    #             del child1.fitness.values
+    #             del child2.fitness.values
+
+    #     for mutant in offspring:
+    #         if np.random.random() < mutpb:
+    #             toolbox.mutate(mutant)
+    #             del mutant.fitness.values
 
         # Vary the pool of individuals
         offspring = algorithms.varAnd(offspring, toolbox, cxpb, mutpb)
@@ -584,6 +596,51 @@ def find_top_predictions(model, model_name, layer_name, teX, teY,
                     imgs_adv[key].reshape(28, 28))
         run_gradcam(model, model_name, imgs[key], orig_targets[key],
                     layer_name)
+
+
+def ga_checkpoint_deap(checkpoint=None):
+    if checkpoint:
+        # A file name has been given, then load the data from the file
+        with open(checkpoint, "r") as cp_file:
+            cp = pickle.load(cp_file)
+        population = cp["population"]
+        start_gen = cp["generation"]
+        halloffame = cp["halloffame"]
+        logbook = cp["logbook"]
+        random.setstate(cp["rndstate"])
+    else:
+        # Start a new evolution
+        population = toolbox.population(n=300)
+        start_gen = 0
+        halloffame = tools.HallOfFame(maxsize=1)
+        logbook = tools.Logbook()
+
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("max", numpy.max)
+
+    for gen in range(start_gen, NGEN):
+        population = algorithms.varAnd(population, toolbox, cxpb=CXPB, mutpb=MUTPB)
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in population if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        halloffame.update(population)
+        record = stats.compile(population)
+        logbook.record(gen=gen, evals=len(invalid_ind), **record)
+
+        population = toolbox.select(population, k=len(population))
+
+        if gen % FREQ == 0:
+            # Fill the dictionary using the dict(key=value[, ...]) constructor
+            cp = dict(population=population, generation=gen, halloffame=halloffame,
+                      logbook=logbook, rndstate=random.getstate())
+
+            with open("checkpoint_name.pkl", "wb") as cp_file:
+                pickle.dump(cp, cp_file)
 
 
 def mse(imgA, imgB):
