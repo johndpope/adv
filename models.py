06d30b6744e8e8cb55b54abf3e173104a97d3b27
kirk86 <kirk86@walla.com>
2017-06-20 14:20:59 +0100

    fixing autoenc model and deleted unecessary cnn_cifar_cifar10.hdf5

--- models.py
+++ models.py
@@ -956,17 +956,21 @@ def conv_ae(data_shape):
     x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
     x = MaxPooling2D((2, 2), padding='same')(x)
     x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-    encoded = MaxPooling2D((2, 2), padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    encoded = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
 
     # at this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-    x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same')(encoded)
     x = UpSampling2D((2, 2))(x)
     x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(16, (3, 3), activation='relu')(x)
+    # x = Conv2D(3, (3, 3), activation='relu', padding='valid')(x)
+    x = Conv2D(3, (3, 3), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
-    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+    # x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    # x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(1, (5, 5), activation='relu', padding='same')(x)
     decoded = Flatten()(decoded)
     decoded = Dense(10, activation='softmax')(decoded)
 
